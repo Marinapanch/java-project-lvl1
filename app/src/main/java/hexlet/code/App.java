572fba3d362c1276c6e46c93cb8ed02c9d8099d5
Marina Panchenko <mpanch128@gmail.com>
2022-09-11 17:27:03 +0300

    make greeting with scanner

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
+//import java.util.Scanner;
+import hexlet.code.Cli;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        var cl = new Cli();
+        cl.makeGreeting();
     }
+   /* public static void main(String[] args) {
+        System.out.println("Welcome to the Brain Games!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String name = sc.nextLine();
+        System.out.println("Hello, " +name +  "!");
+    }
+
+    */
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void makeGreeting() {
+        System.out.println("Welcome to the Brain Games!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
